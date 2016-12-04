f3db9790890d785c25efe2aada34ff47a1870bcb
gnithin <nithin.linkin@gmail.com>
2016-12-04 12:46:13 +0530

    [Q1] Added amendments to help in part 2

--- python/Q1/main.py
+++ python/Q1/main.py
@@ -1,35 +1,33 @@
 import sys
+NORTH, SOUTH, EAST, WEST = 0, 1, 2, 3
+
+
+def get_next_mag_dirn(index, coord, prev_dirn):
+    (dirn, mag) = coord[0], int(coord[1:])
+
+    RCOMPARE, LCOMPARE, YCOMPARE, NCOMPARE = (
+        EAST, WEST, SOUTH, NORTH
+    ) if index % 2 else (
+        NORTH, SOUTH, EAST, WEST
+    )
+
+    if (dirn, prev_dirn) in [('R', RCOMPARE), ('L', LCOMPARE)]:
+        mag = -1 * mag
+        prev_dirn = YCOMPARE
+    else:
+        prev_dirn = NCOMPARE
+    return (mag, prev_dirn)
 
 
 def get_shortest_path_len(coords):
-    NORTH, SOUTH, EAST, WEST = 0, 1, 2, 3
-
-    x_total = 0
-    y_total = 0
-    curr_dirn = NORTH
+    x_total = y_total = 0
+    prev_dirn = NORTH
 
     for index, coord in enumerate(coords):
-        (dirn, mag) = coord[0], int(coord[1:])
-
-        RCOMPARE, LCOMPARE, YCOMPARE, NCOMPARE = (
-            EAST, WEST, SOUTH, NORTH
-        ) if index % 2 else (
-            NORTH, SOUTH, EAST, WEST
-        )
-
-        if (dirn, curr_dirn) in [('R', RCOMPARE), ('L', LCOMPARE)]:
-            mag = -1 * mag
-            curr_dirn = YCOMPARE
-        else:
-            curr_dirn = NCOMPARE
-
-        # print("Mag - " + str(mag) + " dirn " + str(curr_dirn))
-
+        mag, prev_dirn = get_next_mag_dirn(index, coord, prev_dirn)
         if index % 2:
-            # Evaluating vertical movement
             y_total += mag
         else:
-            # Evaluating horizontal movement
             x_total += mag
 
     return abs(x_total) + abs(y_total)
@@ -37,6 +35,7 @@ def get_shortest_path_len(coords):
 if __name__ == "__main__":
     ip_str = [ip.strip() for ip in sys.stdin][0]
     # ip_str = 'R5, L5, R5, R3'
+    # ip_str = 'R8, R4, R4, R8'
     ip_list = ip_str.split(", ")
     # print(ip_list)
     shortest_len = get_shortest_path_len(ip_list)
