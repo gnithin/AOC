8148805ad9dc8c4ccba3634b5f9479c997b4eea5
gnithin <nithin.linkin@gmail.com>
2016-12-05 01:58:23 +0530

    [Q1] Add part 2 for the question

--- python/Q1/main.py
+++ python/Q1/main.py
@@ -19,16 +19,74 @@ def get_next_mag_dirn(index, coord, prev_dirn):
     return (mag, prev_dirn)
 
 
-def get_shortest_path_len(coords):
+def get_all_intermediate_coords(last_coord, ending_coord, prev_dirn):
+    # Messy messy stuff
+    # TODO: Clean this up!
+    intermediate_values = []
+    if prev_dirn in [EAST, WEST]:
+        x2, x1 = ending_coord[0], last_coord[0]
+        y = ending_coord[1]
+        dirn_val = 1
+        if prev_dirn == EAST:
+            dirn_val = -1
+        [
+            intermediate_values.append((new_val, y))
+            for new_val in range(x1 + dirn_val, x2 + dirn_val, dirn_val)
+        ]
+    else:
+        y2, y1 = ending_coord[1], last_coord[1]
+        x = ending_coord[0]
+        dirn_val = 1
+        if prev_dirn == SOUTH:
+            dirn_val = -1
+        [
+            intermediate_values.append((x, new_val))
+            for new_val in range(y1 + dirn_val, y2 + dirn_val, dirn_val)
+        ]
+    return intermediate_values
+
+
+def get_shortest_path_len(coords, find_first_intersection=False):
     x_total = y_total = 0
     prev_dirn = NORTH
+    all_visited_coords = [(0, 0)]
 
     for index, coord in enumerate(coords):
+        # print("-"*20)
+        # print(index, coord)
+
         mag, prev_dirn = get_next_mag_dirn(index, coord, prev_dirn)
         if index % 2:
             y_total += mag
         else:
             x_total += mag
+
+        if find_first_intersection:
+            new_coord = (x_total, y_total)
+            prev_coord = all_visited_coords[-1]
+
+            intermediate_values = get_all_intermediate_coords(
+                prev_coord,
+                new_coord,
+                prev_dirn
+            )
+
+            intersection_flag = False
+            for visited_coord in intermediate_values:
+                if visited_coord in all_visited_coords:
+                    intersection_flag = True
+                    # print("For line between - ", prev_coord, new_coord)
+                    # print(
+                    # "Point in line for ", visited_coord,
+                    # " is found? - ", intersection_flag
+                    # )
+                    break
+            if intersection_flag:
+                x_total, y_total = visited_coord
+                break
+            else:
+                all_visited_coords.extend(intermediate_values)
+            # print(all_visited_coords)
 
     return abs(x_total) + abs(y_total)
 
@@ -37,6 +95,15 @@ if __name__ == "__main__":
     # ip_str = 'R5, L5, R5, R3'
     # ip_str = 'R8, R4, R4, R8'
     ip_list = ip_str.split(", ")
-    # print(ip_list)
+
     shortest_len = get_shortest_path_len(ip_list)
     print("Shortest len - ", shortest_len)
+
+    shortest_len_intersect = get_shortest_path_len(ip_list, True)
+    print("Shortest len with intersections - ", shortest_len_intersect)
+
+    print("For future changes")
+    if (shortest_len_intersect, shortest_len) == (116, 241):
+        print("All succeeded")
+    else:
+        print("All failed! :(")
