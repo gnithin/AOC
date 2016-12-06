6f9d96c5b7df20382ce1c50f2aee200cd56151ae
Nithin <nithin.linkin@gmail.com>
2016-12-06 19:52:56 +0530

    [fix] Add bugfix for question 2

--- python/Q2/main.py
+++ python/Q2/main.py
@@ -4,11 +4,15 @@ DEFAULT_NUM_PAD = ['123', '456', '789']
 INVALID_CHAR = '*'
 
 
-def get_final_code(ip_str):
-    curr_pos = (1, 1)
+def get_final_code(ip_str, initial_pos=(1, 1), custom_num_pad=None):
+    curr_pos = initial_pos
     final_code = ''
     for ip in ip_str:
-        curr_code, curr_pos = get_numpad_code(ip, curr_pos)
+        curr_code, curr_pos = get_numpad_code(
+            ip,
+            initial_pos=curr_pos,
+            custom_num_pad=custom_num_pad
+        )
         final_code += curr_code
     return final_code
 
@@ -22,8 +26,7 @@ def get_numpad_code(ip, initial_pos=(1, 1), custom_num_pad=None):
         'R': (1, 0),
     }
 
-    if custom_num_pad is None:
-        num_pad = DEFAULT_NUM_PAD
+    num_pad = DEFAULT_NUM_PAD if not custom_num_pad else custom_num_pad
 
     max_num_pad_len = len(num_pad[0])
     curr_pos = initial_pos
@@ -39,7 +42,7 @@ def get_numpad_code(ip, initial_pos=(1, 1), custom_num_pad=None):
 
         if (
             not invalid and
-            num_pad[curr_pos[1]][curr_pos[0]] != INVALID_CHAR
+            num_pad[new_pos[1]][new_pos[0]] != INVALID_CHAR
         ):
                 curr_pos = new_pos
                 print(curr_pos)
@@ -56,3 +59,19 @@ if __name__ == "__main__":
     ip_str = [ip.strip() for ip in sys.stdin]
     final_code = get_final_code(ip_str)
     print("Final code -", final_code)
+
+    modified_initial_position = (2, 0)
+    modified_num_pad = [
+        "**1**",
+        "*234*",
+        "56789",
+        "*ABC*",
+        "**D**",
+    ]
+
+    final_modified_code = get_final_code(
+        ip_str,
+        initial_pos=modified_initial_position,
+        custom_num_pad=modified_num_pad
+    )
+    print("Final code -", final_modified_code)
