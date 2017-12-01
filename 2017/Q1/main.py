566d33d35b2aac470c92b9de451bd0eed3b9197d
Nithin <nithin.linkin@gmail.com>
2017-12-01 22:57:57 +0530

    [Q1] Add solution to p2

--- 2017/Q1/main.py
+++ 2017/Q1/main.py
@@ -1,11 +1,11 @@
 import sys
 
 
-def find_sum_of_repeat_digits_in_seq_str(seq_str):
+def find_sum_of_repeat_digits_in_seq_str(seq_str, consecutive_distance):
     seq_str_len = len(seq_str)
     computed_sum = 0
     for curr_index in xrange(seq_str_len):
-        next_index = (curr_index+1) % seq_str_len
+        next_index = (curr_index+consecutive_distance) % seq_str_len
         if seq_str[curr_index] == seq_str[next_index]:
             computed_sum += int(seq_str[curr_index])
     return computed_sum
@@ -13,5 +13,9 @@ def find_sum_of_repeat_digits_in_seq_str(seq_str):
 
 if __name__ == "__main__":
     ip = [ip.strip() for ip in sys.stdin if ip.strip() != ""][0]
-    sum_val = find_sum_of_repeat_digits_in_seq_str(ip)
-    print("The sum of repeated digits - " + str(sum_val))
+    # P1
+    sum_val = find_sum_of_repeat_digits_in_seq_str(ip, 1)
+    print("P1: The sum of repeated digits - " + str(sum_val))
+    # P2
+    sum_val = find_sum_of_repeat_digits_in_seq_str(ip, len(ip)/2)
+    print("P2: The sum of repeated digits - " + str(sum_val))
