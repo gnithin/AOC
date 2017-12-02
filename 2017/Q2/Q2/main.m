42aef0ff44dae5162daa7bff035a94f093be01a8
Nithin <nithin.linkin@gmail.com>
2017-12-02 10:26:53 +0530

    [Q2] Add the basic template for the cmd-line proj

---
+++ 2017/Q2/Q2/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Q2
+//
+//  Created by nithin.g on 02/12/17.
+//  Copyright © 2017 nithin.g. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
