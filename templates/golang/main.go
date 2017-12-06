6a8d88cca87462be0184cb05eb476e675f66ef0e
Nithin <nithin.linkin@gmail.com>
2017-12-06 23:29:00 +0530

    [templates] Add default golang templates for quicker writing startup

---
+++ templates/golang/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"strings"
+)
+
+func main() {
+	filename := "ip.txt"
+	ipList := getIpListFromFilename(filename)
+	fmt.Printf("Input - [%s]\n", strings.Join(ipList, ", "))
+}
+
+func getIpListFromFilename(filename string) []string {
+	ipListBytes, err := ioutil.ReadFile(filename)
+	if err != nil {
+		panic(err)
+	}
+	fileContents := string(ipListBytes)
+	fileContents = strings.TrimSpace(fileContents)
+	ipStrList := strings.Split(fileContents, "\n")
+	return ipStrList
+}
