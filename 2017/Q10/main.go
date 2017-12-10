f759a3667606ccd490e5022792fe9806a1da923a
Nithin <nithin.linkin@gmail.com>
2017-12-10 12:37:38 +0530

    [Q10] Add p2 solution

--- 2017/Q10/main.go
+++ 2017/Q10/main.go
@@ -11,21 +11,17 @@ import (
 func performTransformations(
 	ipArrOriginal, lenList []int,
 	currPos, skip int,
-) []int {
+) ([]int, int, int) {
 	ipArr := make([]int, len(ipArrOriginal))
 	copy(ipArr, ipArrOriginal)
 
 	for _, currLen := range lenList {
-		//fmt.Println("*******")
-		//fmt.Println("Pos ", currPos, " len ", currLen)
-		//fmt.Println("Original ", ipArr)
 		reverseSlice(ipArr, currPos, currLen)
-		//fmt.Println("Reversed ", ipArr)
 		currPos = (currPos + currLen + skip) % len(ipArr)
 		skip += 1
 	}
 
-	return ipArr
+	return ipArr, currPos, skip
 }
 
 func reverseSlice(ipSlice []int, currPos, currLen int) {
@@ -49,14 +45,93 @@ func reverseSlice(ipSlice []int, currPos, currLen int) {
 	}
 }
 
+// Part 2
+func performKnotHash(ipArr, lenList []int) string {
+	pos := 0
+	skip := 0
+	numRounds := 64
+
+	for i := 0; i < numRounds; i++ {
+		ipArr, pos, skip = performTransformations(ipArr, lenList, pos, skip)
+	}
+	return convertIntListToHex(createDenseHashFromSparseHash(ipArr))
+}
+
+func createDenseHashFromSparseHash(sparseHash []int) []int {
+	divisor := 16
+	if len(sparseHash)%divisor != 0 {
+		panic("Invalid sparse hash!")
+	}
+
+	var denseHash []int
+
+	for i := 0; i < len(sparseHash); i += divisor {
+		selectedSlice := sparseHash[i : i+divisor]
+
+		// Find the xor of slice
+		finalXor := selectedSlice[0]
+		for j := 1; j < len(selectedSlice); j++ {
+			finalXor = finalXor ^ selectedSlice[j]
+		}
+		denseHash = append(denseHash, finalXor)
+	}
+
+	return denseHash
+}
+
+func convertIntListToHex(intList []int) string {
+	var hexStringList []string
+	for _, ip := range intList {
+		ipHex := strconv.FormatInt(int64(ip), 16)
+		if len(ipHex) == 1 {
+			ipHex = "0" + ipHex
+		}
+		hexStringList = append(hexStringList, ipHex)
+	}
+
+	return strings.Join(hexStringList, "")
+}
+
 // Main
 func main() {
 	filename := "ip.txt"
 	ipArrLen := 256
 
-	//filename := "trial.txt"
-	//ipArrLen := 5
+	part1(filename, ipArrLen)
+	part2(filename, ipArrLen)
+}
 
+// Part 2
+func part2(filename string, ipArrLen int) {
+	lenList := getFormattedIpListFromFilename(filename)
+	var ipArr []int
+	for i := 0; i < ipArrLen; i++ {
+		ipArr = append(ipArr, i)
+	}
+
+	hashStr := performKnotHash(ipArr, lenList)
+	fmt.Println("Final knot hash -", hashStr)
+}
+
+func getFormattedIpListFromFilename(filename string) []int {
+	ipListBytes, err := ioutil.ReadFile(filename)
+	if err != nil {
+		panic(err)
+	}
+	fileContents := string(ipListBytes)
+	fileContents = strings.TrimSpace(fileContents)
+
+	var ipIntList []int
+	for _, c := range fileContents {
+		ipIntList = append(ipIntList, int(c))
+	}
+
+	ipIntList = append(ipIntList, []int{17, 31, 73, 47, 23}...)
+	return ipIntList
+}
+
+// Part 1
+func part1(filename string, ipArrLen int) {
 	lenList := getIpListFromFilename(filename)
 	var ipArr []int
 	for i := 0; i < ipArrLen; i++ {
@@ -65,9 +140,8 @@ func main() {
 	startingPos := 0
 	initialSkip := 0
 
-	transformedArr := performTransformations(ipArr, lenList, startingPos, initialSkip)
-	//fmt.Println("Final transformed - ", transformedArr)
-	fmt.Println("Final Product - ", transformedArr[0]*transformedArr[1])
+	transformedArr, _, _ := performTransformations(ipArr, lenList, startingPos, initialSkip)
+	fmt.Println("P1 Final Product - ", transformedArr[0]*transformedArr[1])
 }
 
 func getIpListFromFilename(filename string) []int {
