96c2e0705067e90fa1cd4ad1fc56edffd8b9d2a0
Nithin <nithin.linkin@gmail.com>
2017-12-15 00:55:29 +0530

    [Q14] Separated out the methods intp different files and add basic answer for p2

--- 2017/Q14/main.go
+++ 2017/Q14/main.go
@@ -3,132 +3,9 @@ package main
 import (
 	"fmt"
 	"strconv"
-	"strings"
 )
 
-func performTransformations(
-	ipArrOriginal, lenList []int,
-	currPos, skip int,
-) ([]int, int, int) {
-	ipArr := make([]int, len(ipArrOriginal))
-	copy(ipArr, ipArrOriginal)
-
-	for _, currLen := range lenList {
-		reverseSlice(ipArr, currPos, currLen)
-		currPos = (currPos + currLen + skip) % len(ipArr)
-		skip += 1
-	}
-
-	return ipArr, currPos, skip
-}
-
-func reverseSlice(ipSlice []int, currPos, currLen int) {
-	var selectedSlice []int
-	if currPos+currLen > len(ipSlice) {
-		sliceLenFromStart := currLen - (len(ipSlice) - currPos)
-		selectedSlice = append(ipSlice[currPos:], ipSlice[:sliceLenFromStart]...)
-	} else {
-		selectedSlice = ipSlice[currPos:(currPos + currLen)]
-	}
-	// Reverse the slice
-	lenSlice := len(selectedSlice)
-	for i, j := 0, lenSlice-1; i < lenSlice/2; i, j = i+1, j-1 {
-		selectedSlice[i], selectedSlice[j] = selectedSlice[j], selectedSlice[i]
-	}
-
-	pos := currPos
-	for _, val := range selectedSlice {
-		ipSlice[pos] = val
-		pos = (pos + 1) % len(ipSlice)
-	}
-}
-
-func createDenseHashFromSparseHash(sparseHash []int) []int {
-	divisor := 16
-	if len(sparseHash)%divisor != 0 {
-		panic("Invalid sparse hash!")
-	}
-
-	var denseHash []int
-
-	for i := 0; i < len(sparseHash); i += divisor {
-		selectedSlice := sparseHash[i : i+divisor]
-
-		// Find the xor of slice
-		finalXor := selectedSlice[0]
-		for j := 1; j < len(selectedSlice); j++ {
-			finalXor = finalXor ^ selectedSlice[j]
-		}
-		denseHash = append(denseHash, finalXor)
-	}
-
-	return denseHash
-}
-
-func convertIntListToHex(intList []int) string {
-	var hexStringList []string
-	for _, ip := range intList {
-		ipHex := strconv.FormatInt(int64(ip), 16)
-		if len(ipHex) == 1 {
-			ipHex = "0" + ipHex
-		}
-		hexStringList = append(hexStringList, ipHex)
-	}
-
-	return strings.Join(hexStringList, "")
-}
-
-func __performKnotHash(ipArr, lenList []int) string {
-	pos := 0
-	skip := 0
-	numRounds := 64
-
-	for i := 0; i < numRounds; i++ {
-		ipArr, pos, skip = performTransformations(ipArr, lenList, pos, skip)
-	}
-	return convertIntListToHex(createDenseHashFromSparseHash(ipArr))
-}
-
-func performKnotHash(ipStr string) string {
-	ipLen := 256
-	var lenList []int
-	for i := 0; i < ipLen; i++ {
-		lenList = append(lenList, i)
-	}
-
-	// Convert the ipstr to ascii values
-	var ipArr = []int{}
-	for _, r := range ipStr {
-		ipArr = append(ipArr, int(r))
-	}
-	ipArr = append(ipArr, []int{17, 31, 73, 47, 23}...)
-	return __performKnotHash(lenList, ipArr)
-}
-
 // Part 1
-func getNumUsedSquares(ipStr string) int {
-	numRows := 128
-	numUsed := 0
-	used := "1"
-
-	for i := 0; i < numRows; i++ {
-		currStr := ipStr + "-" + strconv.Itoa(i)
-		knotHash := performKnotHash(currStr)
-
-		// Convert the hex to binary
-		binStr := hexToBinStr(knotHash)
-
-		// Find num of used
-		for _, r := range binStr {
-			bin := string(r)
-			if bin == used {
-				numUsed += 1
-			}
-		}
-	}
-	return numUsed
-}
-
 func hexToBin(hex string) (string, error) {
 	ui, err := strconv.ParseUint(hex, 16, 64)
 	if err != nil {
@@ -151,11 +28,149 @@ func hexToBinStr(hex string) string {
 	return binStr
 }
 
+func getNumUsedSquares(ipStr string) (int, [][]int) {
+	numRows := 128
+	numUsed := 0
+	used := "1"
+
+	var rowwiseUsedIndicies [][]int
+
+	for i := 0; i < numRows; i++ {
+		var usedIndices []int
+		currStr := ipStr + "-" + strconv.Itoa(i)
+		knotHash := performKnotHash(currStr)
+
+		// Convert the hex to binary
+		binStr := hexToBinStr(knotHash)
+
+		// Find num of used
+		for ind, r := range binStr {
+			bin := string(r)
+			if bin == used {
+				numUsed += 1
+				usedIndices = append(usedIndices, ind)
+			}
+		}
+
+		rowwiseUsedIndicies = append(rowwiseUsedIndicies, usedIndices)
+	}
+	return numUsed, rowwiseUsedIndicies
+}
+
+// Part 2
+type Coords struct {
+	row int
+	col int
+}
+
+func getNumRegions(ipKey string) int {
+	_, rowwiseUsedIndicies := getNumUsedSquares(ipKey)
+	numRegions := 0
+	row := 0
+
+	for row < len(rowwiseUsedIndicies) {
+		usedIndicesList := rowwiseUsedIndicies[row]
+
+		if len(usedIndicesList) == 0 {
+			row += 1
+			continue
+		}
+		currCoords := Coords{row: row, col: usedIndicesList[0]}
+		regionCoords := mapARegionForCoords(currCoords, &rowwiseUsedIndicies)
+
+		if len(regionCoords) == 0 {
+			panic("This shouldn't happen 1")
+		}
+
+		// Remove all the region coords from the rowwiseUsedIndices
+		for _, regionCoord := range regionCoords {
+			colsList := rowwiseUsedIndicies[regionCoord.row]
+			colPos := find(regionCoord.col, colsList)
+			if colPos == -1 {
+				panic("This shouldn't happen 2")
+			}
+			colsList = append(colsList[:colPos], colsList[colPos+1:]...)
+			rowwiseUsedIndicies[regionCoord.row] = colsList
+		}
+
+		numRegions += 1
+	}
+
+	return numRegions
+}
+
+func mapARegionForCoords(currCoords Coords, rowwiseUsedIndicies *[][]int) []*Coords {
+	var regionQueue []*Coords
+	regionQueue = append(regionQueue, &currCoords)
+	var regionCoords []*Coords
+
+	for len(regionQueue) != 0 {
+		// Pop from region queue
+		poppedCoordsPtr := regionQueue[0]
+		regionQueue = regionQueue[1:]
+
+		// All coords
+		leftCoord := Coords{
+			row: poppedCoordsPtr.row,
+			col: poppedCoordsPtr.col - 1,
+		}
+		rightCoord := Coords{
+			row: poppedCoordsPtr.row,
+			col: poppedCoordsPtr.col + 1,
+		}
+		topCoord := Coords{
+			row: poppedCoordsPtr.row - 1,
+			col: poppedCoordsPtr.col,
+		}
+		bottomCoord := Coords{
+			row: poppedCoordsPtr.row + 1,
+			col: poppedCoordsPtr.col,
+		}
+		for _, c := range []Coords{leftCoord, rightCoord, topCoord, bottomCoord} {
+			if findCoord(c, rowwiseUsedIndicies) {
+				regionCoords = append(regionCoords, &c)
+			}
+		}
+	}
+
+	return regionCoords
+}
+
+func findCoord(coords Coords, rowwiseUsedIndicies *[][]int) bool {
+	if coords.row < 0 || coords.col < 0 {
+		return false
+	}
+
+	totalNumRows := len(*rowwiseUsedIndicies)
+	if coords.row >= totalNumRows {
+		return false
+	}
+
+	colsList := (*rowwiseUsedIndicies)[coords.row]
+	if len(colsList) == 0 {
+		return false
+	}
+	pos := find(coords.col, colsList)
+	return pos != -1
+}
+
+func find(needle int, haystack []int) int {
+	for pos, val := range haystack {
+		if val == needle {
+			return pos
+		}
+	}
+	return -1
+}
+
 // Main
 func main() {
-	//ipKey := "flqrgnkx"
-	ipKey := "jxqlasbh"
+	ipKey := "flqrgnkx"
+	//ipKey := "jxqlasbh"
 	fmt.Println("Ip key - ", ipKey)
-	used := getNumUsedSquares(ipKey)
-	fmt.Println("Used squares - ", used)
+	used, _ := getNumUsedSquares(ipKey)
+	fmt.Println("P1 : Used squares - ", used)
+
+	numRegions := getNumRegions(ipKey)
+	fmt.Println("P2 : Number of regions - ", numRegions)
 }
