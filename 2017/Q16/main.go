56df7d0d5e9fbfbd3be7ef6d7474611b0ed38902
Nithin <nithin.linkin@gmail.com>
2017-12-16 20:45:49 +0530

    [Q16] Add final p2 answer
    commit ccddd3a5705fc5ee3ef5400136507e948e1a8190
    Author: Nithin <nithin.linkin@gmail.com>
    Date:   Sat Dec 16 20:24:16 2017 +0530
    
        [optimize] Find the cycle and use mod to find the result
    
    commit bac7fb71fe578b53eefaf3862d5fd6b65c938185
    Author: Nithin <nithin.linkin@gmail.com>
    Date:   Sat Dec 16 16:39:28 2017 +0530
    
        [optimize] Add enums instead of strings
    
    commit 3fc30e5b6e3c35acbca89fb5b43f3ef305a2b5d2
    Author: Nithin <nithin.linkin@gmail.com>
    Date:   Sat Dec 16 16:31:52 2017 +0530
    
        [optimize] Remove unnecessary function calls
    
    commit 08d27bc1aab1046e16e9cb2a2e6e3ee61649c0a0
    Author: Nithin <nithin.linkin@gmail.com>
    Date:   Sat Dec 16 16:04:16 2017 +0530
    
        [optimize] Perform the parsing once and remove unnecessary small methods

--- 2017/Q16/main.go
+++ 2017/Q16/main.go
@@ -8,60 +8,69 @@ import (
 )
 
 // Part 1
+
+type CmdType int
+
+const (
+	S = iota
+	X
+	P
+)
+
 type CmdManager struct {
 	cmdList []Cmd
 	ipList  []rune
+	cache   []string
 }
 
-func (manager *CmdManager) execute() {
+func (manager *CmdManager) execute() bool {
+	currConfig := string(manager.ipList)
+	cachePos := manager.findPosInCache(currConfig)
+	if cachePos != -1 {
+		manager.cache = manager.cache[:len(manager.cache)-1]
+		nextElemStr := manager.cache[(cachePos+1)%len(manager.cache)]
+		var updatedRuneList []rune
+		for _, r := range nextElemStr {
+			updatedRuneList = append(updatedRuneList, r)
+		}
+		manager.ipList = updatedRuneList
+		return true
+	}
+
 	for _, cmd := range manager.cmdList {
-		manager.execCmd(cmd)
-	}
-}
+		args1 := cmd.args1
+		args2 := cmd.args2
+		switch cmd.name {
+		case S:
+			pivot := len(manager.ipList) - cmd.args1
+			manager.ipList = append(manager.ipList[pivot:], manager.ipList[:pivot]...)
 
-func (manager *CmdManager) execCmd(cmd Cmd) {
-	// Parse the command
-	switch cmd.name {
-	case "s":
-		size, _ := strconv.Atoi(cmd.args)
-		manager.cmdSpin(size)
+		case X:
+			manager.ipList[args1], manager.ipList[args2] = manager.ipList[args2], manager.ipList[args1]
 
-	case "x":
-		posStrList := strings.Split(cmd.args, "/")
-		var posList []int
-		for _, posStr := range posStrList {
-			posInt, _ := strconv.Atoi(string(posStr))
-			posList = append(posList, posInt)
+		case P:
+			pos1 := manager.findPos(rune(args1))
+			pos2 := manager.findPos(rune(args2))
+			manager.ipList[pos1], manager.ipList[pos2] = manager.ipList[pos2], manager.ipList[pos1]
 		}
-		manager.cmdExchange(posList[0], posList[1])
-
-	case "p":
-		posStrList := strings.Split(cmd.args, "/")
-		var posList []rune
-		for _, posStr := range posStrList {
-			posList = append(posList, []rune(posStr)[0])
-		}
-		manager.cmdPartner(posList[0], posList[1])
 	}
 
+	currList := string(manager.ipList)
+	manager.cache = append(manager.cache, currList)
+
+	return false
 }
 
-func (manager *CmdManager) cmdSpin(size int) {
-	//fmt.Println("Spin - ", size)
-	pivot := len(manager.ipList) - size
-	manager.ipList = append(manager.ipList[pivot:], manager.ipList[:pivot]...)
-}
-
-func (manager *CmdManager) cmdExchange(pos1, pos2 int) {
-	//fmt.Println("Exchange - ", pos1, pos2)
-	manager.ipList[pos2], manager.ipList[pos1] = manager.ipList[pos1], manager.ipList[pos2]
-}
-
-func (manager *CmdManager) cmdPartner(name1, name2 rune) {
-	//fmt.Println("Partner - ", string(name1), string(name2))
-	pos1 := manager.findPos(name1)
-	pos2 := manager.findPos(name2)
-	manager.cmdExchange(pos1, pos2)
+func (manager *CmdManager) findPosInCache(needle string) int {
+	if len(manager.cache) == 0 {
+		return -1
+	}
+	for i, entry := range manager.cache[:(len(manager.cache) - 1)] {
+		if entry == needle {
+			return i
+		}
+	}
+	return -1
 }
 
 func (manager *CmdManager) findPos(needle rune) int {
@@ -83,15 +92,14 @@ func (manager *CmdManager) printList() {
 
 // Main
 type Cmd struct {
-	name string
-	args string
+	name  CmdType
+	args1 int
+	args2 int
 }
 
 func main() {
 	filename := "ip.txt"
 	numEntries := 16
-	//filename := "trial.txt"
-	//numEntries := 5
 
 	cmdList := getCmdListFromFilename(filename)
 
@@ -114,7 +122,7 @@ func main() {
 
 	// Part 2
 	fmt.Println("*****P2*****")
-	runCount := 1000000000
+	runCount := int64(1000000000)
 	var p2List = make([]rune, len(ipList))
 	copy(p2List, ipList)
 	p2Manager := CmdManager{
@@ -122,10 +130,21 @@ func main() {
 		ipList:  p2List,
 	}
 
-	for i := 0; i < runCount; i++ {
-		p2Manager.execute()
+	foundCycle := false
+	for i := int64(0); i < runCount; i++ {
+		foundCycle = p2Manager.execute()
+		if foundCycle {
+			break
+		}
 	}
-	p2Manager.printList()
+
+	fmt.Println("Final - runcount - ", runCount)
+	if foundCycle {
+		index := (runCount - 1) % int64(len(p2Manager.cache))
+		fmt.Println(p2Manager.cache[int(index)])
+	} else {
+		p2Manager.printList()
+	}
 }
 
 func getCmdListFromFilename(filename string) []Cmd {
@@ -140,11 +159,39 @@ func getCmdListFromFilename(filename string) []Cmd {
 	for _, ipStr := range ipStrList {
 		cmdStr := string(ipStr[0])
 		args := ipStr[1:]
-		cmd := Cmd{
-			name: cmdStr,
-			args: args,
-		}
-		cmdList = append(cmdList, cmd)
+		cmdList = append(cmdList, parseCommand(cmdStr, args))
 	}
 	return cmdList
+}
+
+func parseCommand(name, args string) Cmd {
+	cmd := Cmd{}
+	switch name {
+	case "s":
+		cmd.name = S
+		size, _ := strconv.Atoi(args)
+		cmd.args1 = size
+
+	case "x":
+		cmd.name = X
+		posStrList := strings.Split(args, "/")
+		var posList []int
+		for _, posStr := range posStrList {
+			posInt, _ := strconv.Atoi(string(posStr))
+			posList = append(posList, posInt)
+		}
+		cmd.args1 = posList[0]
+		cmd.args2 = posList[1]
+
+	case "p":
+		cmd.name = P
+		posStrList := strings.Split(args, "/")
+		var posList []rune
+		for _, posStr := range posStrList {
+			posList = append(posList, []rune(posStr)[0])
+		}
+		cmd.args1 = int(posList[0])
+		cmd.args2 = int(posList[1])
+	}
+	return cmd
 }
