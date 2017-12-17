6920f86f450ee289c394c3f3d1b1ff1ab839a81d
Nithin <nithin.linkin@gmail.com>
2017-12-17 14:20:04 +0530

    [Q17] Solution to p2

--- 2017/Q17/main.go
+++ 2017/Q17/main.go
@@ -13,6 +13,7 @@ type SpinLock struct {
 
 func (self *SpinLock) performLock() int {
 	if len(self.buffer) == 0 {
+		self.buffer = make([]int, 0, 50000000)
 		self.buffer = append(self.buffer, 0)
 	}
 
@@ -20,16 +21,19 @@ func (self *SpinLock) performLock() int {
 		newPos := (self.currPos+self.stepCount)%len(self.buffer) + 1
 		self.insertAtPos(currCounter, newPos)
 		self.currPos = newPos
-
-		//fmt.Println("******")
-		//fmt.Println("Curr count - ", self.currPos)
-		//fmt.Println(self.buffer)
 	}
 	return self.buffer[(self.currPos+1)%len(self.buffer)]
 }
-func (self *SpinLock) findValAfterElement(element int) int {
-	index := self.findValPos(0)
-	return self.buffer[(index+1)%len(self.buffer)]
+func (self *SpinLock) findValAfterInitialElement() int {
+	val := -1
+	for currCounter := 1; currCounter <= self.countLimit; currCounter++ {
+		newPos := (self.currPos+self.stepCount)%(currCounter) + 1
+		self.currPos = newPos
+		if self.currPos == 1 {
+			val = currCounter
+		}
+	}
+	return val
 }
 
 func (self *SpinLock) insertAtPos(val, pos int) {
@@ -45,14 +49,11 @@ func (self *SpinLock) findValPos(needle int) int {
 		}
 	}
 	return -1
-
 }
 
 // Main
 func main() {
 	stepper := 344
-	//stepper := 3
-
 	limit := 2017
 
 	s := SpinLock{
@@ -63,13 +64,10 @@ func main() {
 	fmt.Println("Final value - ", finalVal)
 
 	// Part 2
-	/*
-		p2Limit := 50000000
-		p2SpinLock := SpinLock{
-			stepCount:  stepper,
-			countLimit: p2Limit,
-		}
-		p2SpinLock.performLock()
-		fmt.Println("Value after 0 ", p2SpinLock.findValAfterElement(0))
-	*/
+	p2Limit := 50000000
+	p2SpinLock := SpinLock{
+		stepCount:  stepper,
+		countLimit: p2Limit,
+	}
+	fmt.Println("Value after the initial element -", p2SpinLock.findValAfterInitialElement())
 }
