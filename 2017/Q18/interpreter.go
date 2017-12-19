16ce0466a48fe744121c40c240ca26d53fb9674d
Nithin <nithin.linkin@gmail.com>
2017-12-19 21:32:10 +0530

    [Q18] Incomplete solution of q18

---
+++ 2017/Q18/interpreter.go
@@ -0,0 +1,159 @@
+package main
+
+import (
+	"fmt"
+)
+
+const (
+	INSTN_SET   = "set"
+	INSTN_ADD   = "add"
+	INSTN_MUL   = "mul"
+	INSTN_MOD   = "mod"
+	INSTN_SND   = "snd"
+	INSTN_RCV   = "rcv"
+	INSTN_JGZ   = "jgz"
+	REG_PROG_ID = "p"
+)
+
+// Holds the instruction details
+type Instruction struct {
+	cmdName  string
+	register string
+	argument interface{}
+}
+
+// Interprets the instruction set
+type Interpreter struct {
+	instnList                 []Instruction
+	varEnv                    map[string]int
+	instnIndex                int
+	readingChan               chan int
+	writingChan               chan int
+	isReadingStatusForReading *bool
+	isReadingStatusForWriting *bool
+	sendCount                 int
+	responseChan              chan int
+	stopInterrupt             bool
+	progId                    int
+}
+
+func createInterpreter(
+	progId int,
+	instnList []Instruction,
+	readingChan,
+	writingChan chan int,
+	isReadingStatusForReading,
+	isReadingStatusForWriting *bool,
+	responseChan chan int,
+) Interpreter {
+	varEnv := make(map[string]int)
+	varEnv[REG_PROG_ID] = progId
+
+	return Interpreter{
+		progId:                    progId,
+		instnList:                 instnList,
+		varEnv:                    varEnv,
+		instnIndex:                0,
+		readingChan:               readingChan,
+		writingChan:               writingChan,
+		isReadingStatusForReading: isReadingStatusForReading,
+		isReadingStatusForWriting: isReadingStatusForWriting,
+		responseChan:              responseChan,
+		stopInterrupt:             false,
+	}
+}
+
+func (self *Interpreter) getValOfRegister(regName string) int {
+	regVal, _ := self.varEnv[regName]
+	return regVal
+}
+
+func (self *Interpreter) setValToRegister(regName string, val int) {
+	self.varEnv[regName] = val
+}
+
+func (self *Interpreter) run() {
+	for self.instnIndex >= 0 && self.instnIndex < len(self.instnList) {
+		shouldStop := self.runInstn()
+		if shouldStop || self.stopInterrupt == true {
+			//self.responseChan <- self.varEnv[REG_PROG_ID]
+			break
+		}
+	}
+}
+
+func (self *Interpreter) runInstn() bool {
+	currInstn := self.instnList[self.instnIndex]
+	register := currInstn.register
+
+	argVal := 0
+	switch tempArgVal := currInstn.argument.(type) {
+	case int:
+		argVal = tempArgVal
+	case string:
+		argVal = self.getValOfRegister(tempArgVal)
+		//register = tempArgVal
+	case nil:
+		// Pass
+	default:
+		panic("Unknown type for argument")
+	}
+
+	//fmt.Println(self.progId, currInstn.cmdName, register, currInstn.argument, argVal)
+	//fmt.Println(self.progId, currInstn.cmdName, currInstn.register, currInstn.argument)
+
+	// Add the switch cases here
+	switch currInstn.cmdName {
+	case INSTN_SET:
+		self.setValToRegister(register, argVal)
+		self.instnIndex += 1
+
+	case INSTN_ADD:
+		oldVal := self.getValOfRegister(register)
+		newVal := oldVal + argVal
+		self.setValToRegister(register, newVal)
+		self.instnIndex += 1
+
+	case INSTN_MUL:
+		oldVal := self.getValOfRegister(register)
+		newVal := oldVal * argVal
+		self.setValToRegister(register, newVal)
+		self.instnIndex += 1
+
+	case INSTN_MOD:
+		oldVal := self.getValOfRegister(register)
+		//fmt.Println(oldVal, argVal)
+		newVal := oldVal % argVal
+		self.setValToRegister(register, newVal)
+		self.instnIndex += 1
+
+	case INSTN_SND:
+		self.sendCount += 1
+		//fmt.Println(self.progId, "sending - ", argVal)
+		self.writingChan <- argVal
+		self.instnIndex += 1
+
+	case INSTN_RCV:
+		if self.progId == 1 {
+			fmt.Println("Reg val - ", self.sendCount)
+		}
+		(*self.isReadingStatusForWriting) = true
+		newVal := <-self.readingChan
+		//fmt.Println(self.progId, "Received - ", newVal)
+		self.setValToRegister(register, newVal)
+		self.instnIndex += 1
+
+	case INSTN_JGZ:
+		regVal := self.getValOfRegister(register)
+		if regVal > 0 {
+			self.instnIndex += argVal
+		} else {
+			self.instnIndex += 1
+		}
+
+	default:
+		panic("Unknown instruction!")
+	}
+
+	return false
+}
--- 2017/Q18/main.go
+++ 2017/Q18/main.go
@@ -13,13 +13,65 @@ import (
 func main() {
 	filename := "ip.txt"
 	//filename := "trial.txt"
+	//filename := "p2.txt"
 	instnList := getIpListFromFilename(filename)
 
-	// Part 1
-	interpreter := createInterpreter(instnList)
-	interpreter.run()
-	recoveredFreq := interpreter.recoveredFreq
-	fmt.Println("Recovered freq - ", recoveredFreq)
+	/*
+		For part-1, rename the executor.go.bak to executor.go
+		and uncomment this part, and rename interpreter.go to
+		interpreter.go.bak and comment out part-2
+	*/
+	/*
+		// Part 1
+		interpreter := createInterpreter(instnList)
+		interpreter.run()
+		recoveredFreq := interpreter.recoveredFreq
+		fmt.Println("Recovered freq - ", recoveredFreq)
+	*/
+
+	// Part 2
+	size := 10000000
+	ch1 := make(chan int, size)
+	ch2 := make(chan int, size)
+	responseChan := make(chan int)
+	flagValue1 := false
+	flagValue2 := false
+
+	interpreter0 := createInterpreter(
+		0,
+		instnList,
+		ch1,
+		ch2,
+		&flagValue1,
+		&flagValue2,
+		responseChan,
+	)
+
+	interpreter1 := createInterpreter(
+		1,
+		instnList,
+		ch2,
+		ch1,
+		&flagValue2,
+		&flagValue1,
+		responseChan,
+	)
+
+	go func() {
+		interpreter0.run()
+	}()
+	go func() {
+		interpreter1.run()
+	}()
+
+	progId := <-responseChan
+	if progId == 0 {
+		interpreter1.stopInterrupt = true
+	} else {
+		interpreter0.stopInterrupt = true
+	}
+	_ = <-responseChan
+	fmt.Println("Send value of interpreter 1 -", interpreter1.sendCount)
 }
 
 func getIpListFromFilename(filename string) []Instruction {
