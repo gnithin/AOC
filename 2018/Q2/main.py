d31c25aa482dd0a8df2be16fedb09d57cbcfb411
Nithin <nithin.linkin@gmail.com>
2018-12-02 20:25:43 +0530

    [Q2] Solution to part -2

--- 2018/Q2/main.py
+++ 2018/Q2/main.py
@@ -2,9 +2,7 @@ def find_checksum(ids):
     num_3 = 0
     num_2 = 0
     for entry in ids:
-        print(entry)
         freq_map = find_freq(entry)
-        print(freq_map)
         temp_freq_store = [0, 0]
         for c in freq_map:
             freq = freq_map[c]
@@ -27,14 +25,35 @@ def find_freq(entry):
     return freq_map
 
 
+def find_common(ids):
+    for i in range(0, len(ids) - 1):
+        parent = ids[i]
+        for j in range(i+1, len(ids)):
+            child = ids[j]
+            # Compare diff
+            common_char = []
+            num_diff = 0
+
+            for ci in range(0, len(parent)):
+                if parent[ci] == child[ci]:
+                    common_char.append(parent[ci])
+                else:
+                    num_diff += 1
+            if num_diff == 1:
+                return common_char
+
+
 if __name__ == "__main__":
     # file_name = "trial_ip.txt"
+    # file_name = "trial_ip_2.txt"
     file_name = "ip.txt"
     with open(file_name, 'r') as fp:
         contents = fp.read()
     ip_contents = [ip.strip() for ip in contents.strip().split("\n")
                    if ip.strip() != ""]
-    print(ip_contents)
+    # print(ip_contents)
     checksum = find_checksum(ip_contents)
     print("Checksum -")
     print(checksum)
+
+    print("".join(find_common(ip_contents)))
